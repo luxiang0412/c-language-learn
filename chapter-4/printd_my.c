52e8720b52593c6ccfa730230ca19c5a8f83dbdb
luxiang0412 <1374920889@qq.com>
2018-09-17 14:00:37 +0800

    printd

--- chapter-4/printd_my.c
+++ chapter-4/printd_my.c
@@ -3,4 +3,16 @@
 void main(int n){
 	printf("%d\n",1/10);
 	printf("%d\n",11/10);
+	void printd(int n );
+	printd(213123);
+}
+void printd(int n)
+{
+	if(n<0){
+		putchar('-');
+		n = -n;
+	}
+	if(n/10)
+		printd(n/10);
+	putchar(n%10+'0');
 }
