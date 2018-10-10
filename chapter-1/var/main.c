fbe730d73b2d130a1fdfa1610324ba2d94102e6c
luxiang0412 <1374920889@qq.com>
2018-10-10 12:18:09 +0800

    s

--- chapter-1/var/main.c
+++ chapter-1/var/main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+void c_2(void)
+{
+	printf("main_c_2\n");
+}
+/** c_() 在c.c中被定义不能再被定义
+void c_(void)
+{
+	printf("main_c");
+}
+**/
 void main()
 {
 	//extern int c;
@@ -10,6 +20,7 @@ void main()
 	a_();
 	b_();
 	c_();
+	c_2();
 	//c_2();//error
 	printf("%d\n",a);
 	//printf("%d\n",c);//error static 不可引用
